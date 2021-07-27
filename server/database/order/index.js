dbef3a2be7904f36e004452e30aa04ac6aafe642
gayathri1398 <gayathrivasu13@gmail.com>
2021-07-28 00:11:17 +0530

    Added the model for all collections

---
+++ server/database/order/index.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const OrderSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Types.ObjectId,
+        ref:"Users"
+    },
+    orderDetails: [{
+        food: {
+            type: mongoose.Types.ObjectId,
+            ref: "Foods"
+        },
+     quantity: {type:Number, required: true},
+     payment: {type:String, required: true},
+     status: {type: String, default: "Placed"},
+     paymentDetails: {
+        itemTotal:  {type: Number, required: true},//we cant refer the food model because when the price of food changes, the history details
+                                                    //  also gets changed . 
+        promo: {type: Number, required: true},     // promocode:amount
+        tax: {type: Number, required: true},
+
+    },  
+    }],
+    orderRatings: {type: Number, required:true},
+});
+
+export const OrderModel = mongoose.model("Orders", OrderSchema)
