2d28018a3aa1374097039a4434d5522d5f1e9c24
gayathri1398 <gayathrivasu13@gmail.com>
2021-08-19 16:46:41 +0530

    added the review validation

--- server/API/Reviews/index.js
+++ server/API/Reviews/index.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import passport from 'passport';
 
+// validation
+import { ValidateReview ,ValidateReviewId} from '../../Validation/review';
+
 // Database Models
 import { ReviewModel } from '../../database/reviews';
+
 
 const Router = express.Router();
 
@@ -17,6 +21,7 @@ Method          POST
 
 Router.post("/new", async(req,res)=>{
     try {
+        await ValidateReview(req.body);
         const {reviewData} = req.body;
          await ReviewModel.create(reviewData);  //not returning anything so need not name it with const
         return res.status(200).json({review: "Successfully created review"})
@@ -37,6 +42,7 @@ Method           DELETE
 
 Router.delete("/delete:_id", async(req,res)=>{
     try {
+        await ValidateReviewId(req.params);
         const {_id} = req.params;
          await ReviewModel.findByIdAndDelete(_id); 
         return res.status(200).json({reviews: "Successfully deleted the review"});  
