a725b794d7833389e48ccfd52fc8c37ac67a778f
gayathri1398 <gayathrivasu13@gmail.com>
2021-08-29 20:33:37 +0530

    restaurant layout[pending]

--- client/src/layouts/Restaurant.layout.jsx
+++ client/src/layouts/Restaurant.layout.jsx
@@ -6,10 +6,59 @@ import Navbar from '../components/Navbar/restaurant';
 
 const Restaurantlayout = (props) => {
     return (
-        <div>
+        <>
+        <div className="container mx auto lg:px-40">
             <Navbar/>
             {props.children}
+        {/*grid*/}
+        <div className="w-full h-60 md:hidden">
+            <img src="https://b.zmtcdn.com/data/pictures/chains/0/18512380/9a60985de026ed701b2a2113d42ec8fc.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*" 
+            alt="restaurant food image"
+            className="w-full h-full" />
         </div>
+
+           <div className="w-full border">
+           <div className="flex gap-1 borderw-full h-96">
+           <div className="hidden md:block w-7/12 hover:tranform scale-50">
+            <img src="https://b.zmtcdn.com/data/pictures/chains/0/18512380/9a60985de026ed701b2a2113d42ec8fc.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*" 
+            alt="restaurant food image"
+            className="w-full h-full object-cover" />
+            </div>
+              <div className=" w-1/3 flex gap-1">
+              <div className="flex flex-col gap-1">
+                <div className="hidden md:block w-full h-48">
+                   <img src="https://b.zmtcdn.com/data/pictures/chains/0/18512380/9a60985de026ed701b2a2113d42ec8fc.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*" 
+                    alt="restaurant food image"
+                    className="w-full h-full object-cover" />
+                    </div>
+                    <div className="hidden md:block w-full h-48">
+                    <img src="https://b.zmtcdn.com/data/pictures/chains/0/18512380/9a60985de026ed701b2a2113d42ec8fc.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*" 
+                    alt="restaurant food image"
+                    className="w-full h-full object-cover" />
+                    </div>
+                </div>
+                <div className="flex flex-col gap-1">
+                <div className="hidden md:block w-full h-48">
+                   <img src="https://b.zmtcdn.com/data/pictures/chains/0/18512380/9a60985de026ed701b2a2113d42ec8fc.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*" 
+                    alt="restaurant food image"
+                    className="w-full h-full object-cover" />
+                    </div>
+                    <div className="hidden md:block w-full h-48">
+                    <img src="https://b.zmtcdn.com/data/pictures/chains/0/18512380/9a60985de026ed701b2a2113d42ec8fc.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*" 
+                    alt="restaurant food image"
+                    className="w-full h-full object-cover" />
+                    </div>
+                </div>
+              </div>
+              
+           </div>
+           </div>
+          
+  
+           </div>
+
+      
+</>
     )
 }
 
