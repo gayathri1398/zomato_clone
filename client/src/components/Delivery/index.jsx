6cfa6aa50c32a9fb7f6916520eca2862818a5156
gayathri1398 <gayathrivasu13@gmail.com>
2021-10-07 19:28:50 +0530

    restaurant reducer completed

--- client/src/components/Delivery/index.jsx
+++ client/src/components/Delivery/index.jsx
@@ -7,7 +7,32 @@ import RestaurantCard from '../RestaurantCard';
 
 const DeliveryPage = () => {
   
-    const [restaurantList, setrestaurantList] = useState([]);
+    const [restaurantList, setrestaurantList] = useState([
+        // {
+        //     _id:"123",
+        //     photoss:["https://b.zmtcdn.com/data/pictures/chains/8/65528/6500ebf7d8c9769dbc611e4c524bea3d_o2_featured_v2.jpg?output-format=webp",
+        // "https://b.zmtcdn.com/data/pictures/chains/8/65528/6500ebf7d8c9769dbc611e4c524bea3d_o2_featured_v2.jpg?output-format=webp"],
+        //     isPro:true,
+        //     isOFF:"80",
+        //     durationOfDelivery:"55",
+        //     name:"Pizza hut",
+        //     averageCost:"160",
+        //     cuisions:["burger","pizza"]
+        // },
+        // {
+        //     _id:"123",
+        //     photoss:["https://b.zmtcdn.com/data/pictures/chains/8/65528/6500ebf7d8c9769dbc611e4c524bea3d_o2_featured_v2.jpg?output-format=webp",
+        // "https://b.zmtcdn.com/data/pictures/chains/8/65528/6500ebf7d8c9769dbc611e4c524bea3d_o2_featured_v2.jpg?output-format=webp"],
+        //     isPro:true,
+        //     isOFF:"80",
+        //     durationOfDelivery:"55",
+        //     name:"Pizza hut",
+        //     averageCost:"160",
+        //     cuisions:["burger","pizza"]
+        // },
+  
+
+    ]);
     const reduxState = useSelector(
         (globalStore) => globalStore.restaurant.restaurants
         );
@@ -26,8 +51,8 @@ const DeliveryPage = () => {
         <h1 className="text-gray-800 text-2xl my-6 lg:font-normal text-xl md:text-3xl m-3 px-2 lg:my-6 lg:text-4xl">Sholinganallur Restaurants, Chennai</h1>
        <div className="w-full md:flex flex-wrap justify-evenly">  
            {restaurantList.map((restaurant)=>(
-                 <RestaurantCard{...restaurantList}
-                  key={restaurant._id}
+                 <RestaurantCard{...restaurant}
+                  key={restaurantList._id}
                  />
            ))}
        </div>
--- client/src/components/RestaurantCard.jsx
+++ client/src/components/RestaurantCard.jsx
@@ -3,11 +3,11 @@ import {AiTwotoneStar} from 'react-icons/ai'
 
 const RestaurantCard = (props) => {
     return<>
-     <div className="mb-2 m-4 bg-white rounded-2xl shadow-lg md:w-5/12 lg:shadow-none lg:w-80 hover:shadow-lg transition duration-700 ease-in-out">
+     <div className="mb-2 m-4 bg-white rounded-2xl shadow-lg md:w-5/12 lg:shadow-none lg:w-80 hover:shadow-lg transition duration-700 ease-in-out mb-40">
        
         <div className="relative" >     
             <div className="w-full h-48 p-2 md:h-52 lg:w-80 lg:h-60" >
-                <img src={ props.photos} 
+                <img src={props.photos.length && props.photos[0]} 
                 alt="Food"
                 className="w-full h-full object-cover rounded-t-2xl lg:rounded-2xl" />
             </div>
@@ -17,7 +17,7 @@ const RestaurantCard = (props) => {
                     <h1 className="bg-zomato-400 rounded-r px-1 m-2 text-sm">Pro extra 10% OFF</h1>
                 )}
                {props.isOff && (
-                    <h1 className="bg-blue-400 rounded-r px-1 m-2 text-sm">{`${props.isOff}% OFF`}</h1>
+                    <h1 className="bg-blue-400 rounded-r px-1 m-2 text-sm">₹{`${props.isOff}`}OFF</h1>
                )}
             </div>
                 <span className="bg-white opacity-75 rounded py-1 px-2 text-sm mr-2">{props.durationOfDelivery}min</span>
@@ -26,7 +26,7 @@ const RestaurantCard = (props) => {
        <div className="flex justify-between my-1 mx-2 p-1">
        <div className="flex flex-col">
             <h1 className="text-base">{props.name}</h1>
-            <p className="text-sm text-gray-500">{props.cuisions}</p>
+            <p className="text-sm text-gray-500">{props.cuisions.join(", ")}</p>
         </div>
         <div className="flex flex-col items-end">
             <span className="bg-green-500 text-white flex w-9 text-sm items-center justify-center rounded">{props.restaurantReviewValue}<AiTwotoneStar className="text-sm"/></span>
